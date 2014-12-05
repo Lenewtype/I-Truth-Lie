aee96c8faaf8db4cf9d3ec94832c901c6dd625ab
Nick Miloscia <njm5586@g.rit.edu>
2014-12-05 00:24:35 -0500

    Revert "Added some placeholder templates"
    This reverts commit 7ca89c555fa1b5af044fb61b6d1066027d66c65a.

--- app/home/home.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('iTruthLie.angular.home')
-	.controller('HomeController', function($scope, $location){
-
-		$scope.gotoPage = function(pageName){
-			$location.path('/'+pageName);
-		};
-	});
