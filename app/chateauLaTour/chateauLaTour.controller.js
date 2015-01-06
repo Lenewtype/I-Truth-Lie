172df79b99983cee022c1b29696676ae4cb2e547
Nicholas Miloscia <nmiloscia@sapient.com>
2015-01-06 14:49:12 -0500

    Changes to styles for discography

--- app/chateauLaTour/chateauLaTour.controller.js
+++ app/chateauLaTour/chateauLaTour.controller.js
@@ -1,7 +1,11 @@
 angular.module('iTruthLie.angular.chateauLaTour')
-	.controller('ChateauLaTourController', function($scope, $location){
-
-		$scope.gotoPage = function(pageName){
-			$location.path('/'+pageName);
-		};
+	.controller('ChateauLaTourController', function($scope){
+        $scope.albums = [];
+        $scope.albums['metempsychosis'] = {
+            name: 'Metem Psychosis',
+            tracks: [{title: 'Metem Psychosis'}, {title:'Smile (Album Ver.)'}, {title:'残骸、残想、残酷な貴方へ...', romaji: 'Zangai, Zanzou, Zankokuna Anata e...'},
+            {title: '葬列', romaji: 'Souretsu'}, {title: 'Solitude'}, {title: 'Smile (Single vers.)'}, {title: 'I Wish'}, {title: 'I Truth Lie'}, {title: '奇形ノ君へ', romaji: 'Kikei no Kimi e'},
+            {title: 'Schizopherenia'},{title: 'Tragedy'}, {title: 'Primary'}, {title: 'Reverse'}],
+            albumArt: 'metem.png'
+        };
 	});
