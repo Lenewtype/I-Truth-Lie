68729983dfd0d2e1546ec59c8cc5fa47435acbc2
Nicholas Miloscia <nmiloscia@sapient.com>
2015-02-16 18:03:19 -0500

    Finished Kar'MariA page, some design stuff, and start of DEViL KiTTY

--- app/karMaria/karmaria.controller.js
+++ app/karMaria/karmaria.controller.js
@@ -4,4 +4,53 @@ angular.module('iTruthLie.angular.karMaria')
 		$scope.gotoPage = function(pageName){
 			$location.path('/'+pageName);
 		};
+        $scope.albums = [];
+        $scope.albums['maria'] = {
+            name: 'Maria',
+            type: 'Single',
+            date: '09/26/2001',
+            price: '¥1,575',
+            tracks: [{title: 'Maria'}, {title:'毒舌2ちゃんねラー', romaji:'dokuzetsu 2-chan neraa'}, {title:'Karma'}],
+            albumArt: 'maria.jpg'
+        };
+        $scope.albums['reset'] = {
+            name: 'Reset',
+            type: 'Mini-album',
+            date: '05/30/2001',
+            price: '¥2,100',
+            tracks: [{title: 'Deep Love'}, {title: 'Chain'}, {title: 'a habit'}, {title: 'I than Reset you'}, {title:'約束', romaji:'Yakusoku'}/*, {title:'「」'}*/],
+            albumArt: 'reset.jpg'
+        };
+        $scope.albums['reset2'] = {
+            name: 'Reset (Second Press)',
+            type: 'Mini-album',
+            date: '09/xx/2001',
+            price: '¥2,100',
+            tracks: [{title: 'Deep Love'}, {title: 'Chain'}, {title: 'a habit'}, {title: 'I than Reset you'}, {title:'約束', romaji:'Yakusoku'}, {title:'「」'}],
+            albumArt: 'blank.png'
+        };
+        $scope.albums['untitled'] = {
+            name: '[Untitled]',
+            type: 'Live-distributed Single',
+            date: '09/24/2001',
+            price: '¥0',
+            tracks: [{title: 'マリア', romaji: 'MARIA'}, {title: 'さよなら妄想ちゃん', romaji: 'Sayonara Mousou-chan'}],
+            albumArt: 'blank.png'
+        };
+        $scope.albums['kaleidoscope'] = {
+            name: 'Kaleidoscope',
+            type: 'Omnibus',
+            date: '06/01/2003',
+            price: '¥??',
+            tracks: [{title: '約束', romaji: 'Yakusoku'}],
+            albumArt: 'kaleidoscope.png'
+        };
+        $scope.albums['untitled2'] = {
+            name: '[Untitled]',
+            type: 'Live-distributed Single',
+            date: '09/25/2001',
+            price: '¥0',
+            tracks: [{title: 'あきの曲だゴルァ!!', romaji: 'Aki no Kyoku da GORUa!!'}, {title: 'バラードだゴルァ!!', romaji: 'BALLAD da GORUa!!'}],
+            albumArt: '11_24.png'
+        };
 	});
